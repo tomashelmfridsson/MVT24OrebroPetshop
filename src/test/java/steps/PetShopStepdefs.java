dcf1ec5d67daf4b7ca72c8f92ab0d9d7b491ed94
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-03-22 19:34:59 +0100

    first testcase ok

--- src/test/java/steps/PetShopStepdefs.java
+++ src/test/java/steps/PetShopStepdefs.java
@@ -6,20 +6,18 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PetShopStepdefs{
-    private WebDriver driver = new ChromeDriver();
+    private final WebDriver driver = new ChromeDriver();
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
     @After
